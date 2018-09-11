70e6786a458737d3ef44e921d93d25af4b1fd5d6
lovit <soy.lovit@gmail.com>
2018-09-11 14:41:09 +0900

    Add docstring for Sphinx

--- network_based_nearest_neighbors/index.py
+++ network_based_nearest_neighbors/index.py
@@ -5,6 +5,26 @@ from sklearn.metrics import pairwise_distances
 from .utils import most_closest_points
 
 class NetworkBasedNeighbors:
+    """Network based nearest neighbor search index
+
+    .. code-block:: python
+
+        >>> index = NetworkBasedNeighbors(
+        >>>     x,                     # reference data. numpy.ndarray or scipy.sparse.csr_matrix
+        >>>     n_nearest_neighbors=5, # number of nearest neighbors
+        >>>     n_random_neighbors=5,  # number of random neighbors
+        >>>     batch_size=500,        # indexing batch size
+        >>>     metric='euclidean',    # metric, possible all metric defined in scipy
+        >>>     verbose=True           # verbose mode if True
+        >>> )
+        >>> dist, idxs = index.search_neighbors(query, k=k) # usage mode
+        >>> dist, idxs, infos, process_time = index._search_neighbors_dev(query, k=k) # debug mode
+
+    :param int a: An integer to be computed
+    :param int b: An integer to be computed
+
+    :returns: The sum of two integer values
+    """
     def __init__(self, X=None, n_nearest_neighbors=5,
         n_random_neighbors=5, batch_size=500,
         metric='euclidean', verbose=True):
@@ -20,6 +40,11 @@ class NetworkBasedNeighbors:
             self.index(X)
 
     def index(self, X):
+        """The function for indexing.
+
+        :param Union[scipy.sparse.csr_matrix, numpy.ndarray] x: matrix
+        :returns: None
+        """
         n_data = X.shape[0]
         num_nn = self.n_nearest_neighbors
         num_rn = self.n_random_neighbors
@@ -44,11 +69,31 @@ class NetworkBasedNeighbors:
             print('Indexing was done')
 
     def search_neighbors(self, query, k=5, max_steps=10, converge=0.000001):
+        """Search function.
+
+        :param Union[sparse.csr_matrix, numpy.ndarray] query: query vector
+        :param int k: the number of nearest neighbors
+        :param int max_steps: the maximal number of updates
+        :param float converge: threshold for convergence check
+        :returns: tuple of nearest neighbor distances and corresponding indices
+        """
+
         dist, idxs, infos, process_time = self._search_neighbors_dev(
             query, k, max_steps, converge)
         return dist, idxs
 
     def _search_neighbors_dev(self, query, k=5, max_steps=10, converge=0.000001):
+        """Search function.
+
+        :param Union[sparse.csr_matrix, numpy.ndarray] query: query vector
+        :param int k: the number of nearest neighbors
+        :param int max_steps: the maximal number of updates
+        :param float converge: threshold for convergence check
+        :returns: distances, indices, update logs, process time
+            update logs is list consisting
+            (step index, average distance, distance difference with previous step)
+        """
+
         buffer_size = self.buffer_factor * k
         dist, idxs = self._initialize(query, buffer_size)
         dist_avg = dist.sum() / dist.shape[0]
